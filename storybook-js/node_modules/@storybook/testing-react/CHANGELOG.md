3013f0b4c6d8559811e28147c61c7e1e45d18dad
rosefang <rosefang@tencent.com>
2021-10-30 10:14:09 +0800

    feat: first comit

---
+++ storybook-js/node_modules/@storybook/testing-react/CHANGELOG.md
@@ -0,0 +1,214 @@
+# v0.0.17 (Thu Jun 03 2021)
+
+#### 🐛 Bug Fix
+
+- Widen peer dependency range to include prereleases [#26](https://github.com/storybookjs/testing-react/pull/26) ([@IanVS](https://github.com/IanVS))
+
+#### Authors: 1
+
+- Ian VanSchooten ([@IanVS](https://github.com/IanVS))
+
+---
+
+# v0.0.16 (Mon May 24 2021)
+
+#### 🐛 Bug Fix
+
+- fix: move storybook libs as peer deps [#23](https://github.com/storybookjs/testing-react/pull/23) ([@yannbf](https://github.com/yannbf))
+
+#### Authors: 1
+
+- Yann Braga ([@yannbf](https://github.com/yannbf))
+
+---
+
+# v0.0.15 (Sun May 23 2021)
+
+#### 🐛 Bug Fix
+
+- feat: add args as a result from composed story [#19](https://github.com/storybookjs/testing-react/pull/19) ([@yannbf](https://github.com/yannbf))
+
+#### Authors: 1
+
+- Yann Braga ([@yannbf](https://github.com/yannbf))
+
+---
+
+# v0.0.14 (Sun May 23 2021)
+
+#### 🐛 Bug Fix
+
+- fix: Remove deep import StoryFnReactReturnType from @storybook/react [#20](https://github.com/storybookjs/testing-react/pull/20) ([@ljcl](https://github.com/ljcl))
+
+#### Authors: 1
+
+- Luke Clark ([@ljcl](https://github.com/ljcl))
+
+---
+
+# v0.0.13 (Fri May 21 2021)
+
+#### 🐛 Bug Fix
+
+- fix: ignore __esModules flag when composing stories [#22](https://github.com/storybookjs/testing-react/pull/22) ([@yannbf](https://github.com/yannbf))
+
+#### Authors: 1
+
+- Yann Braga ([@yannbf](https://github.com/yannbf))
+
+---
+
+# v0.0.13 (Fri May 21 2021)
+
+#### 🐛 Bug Fix
+
+- fix: ignore __esModules flag when composing stories [#22](https://github.com/storybookjs/testing-react/pull/22) ([@yannbf](https://github.com/yannbf))
+
+#### Authors: 1
+
+- Yann Braga ([@yannbf](https://github.com/yannbf))
+
+---
+
+# v0.0.12 (Wed May 12 2021)
+
+#### 🐛 Bug Fix
+
+- fix: module field should point to a real file [#21](https://github.com/storybookjs/testing-react/pull/21) ([@elevatebart](https://github.com/elevatebart))
+
+#### Authors: 1
+
+- Barthélémy Ledoux ([@elevatebart](https://github.com/elevatebart))
+
+---
+
+# v0.0.11 (Sat May 08 2021)
+
+#### 🐛 Bug Fix
+
+- fix: throw error on unsupported formats [#18](https://github.com/storybookjs/testing-react/pull/18) ([@yannbf](https://github.com/yannbf))
+
+#### Authors: 1
+
+- Yann Braga ([@yannbf](https://github.com/yannbf))
+
+---
+
+# v0.0.10 (Mon Apr 05 2021)
+
+#### 🐛 Bug Fix
+
+- fix: support addons that use channel api [#14](https://github.com/storybookjs/testing-react/pull/14) ([@yannbf](https://github.com/yannbf))
+
+#### Authors: 1
+
+- Yann Braga ([@yannbf](https://github.com/yannbf))
+
+---
+
+# v0.0.9 (Tue Mar 30 2021)
+
+#### 🐛 Bug Fix
+
+- Fix: properly handle global types support [#11](https://github.com/storybookjs/testing-react/pull/11) ([@yannbf](https://github.com/yannbf))
+
+#### Authors: 1
+
+- Yann Braga ([@yannbf](https://github.com/yannbf))
+
+---
+
+# v0.0.8 (Tue Mar 30 2021)
+
+#### 🐛 Bug Fix
+
+- chore: update package url [#10](https://github.com/storybookjs/testing-react/pull/10) ([@yannbf](https://github.com/yannbf))
+
+#### Authors: 1
+
+- Yann Braga ([@yannbf](https://github.com/yannbf))
+
+---
+
+# v0.0.7 (Tue Mar 30 2021)
+
+#### 🐛 Bug Fix
+
+- Fix: compose stories with partial props [#9](https://github.com/storybookjs/testing-react/pull/9) ([@yannbf](https://github.com/yannbf))
+- Update Button.stories.tsx [#6](https://github.com/storybookjs/testing-react/pull/6) ([@nativedone](https://github.com/nativedone))
+
+#### Authors: 2
+
+- AD Amorim ([@nativedone](https://github.com/nativedone))
+- Yann Braga ([@yannbf](https://github.com/yannbf))
+
+---
+
+# v0.0.6 (Mon Mar 22 2021)
+
+#### 🐛 Bug Fix
+
+- Update README.md [#5](https://github.com/storybookjs/testing-react/pull/5) ([@diegohaz](https://github.com/diegohaz))
+
+#### Authors: 1
+
+- Haz ([@diegohaz](https://github.com/diegohaz))
+
+---
+
+# v0.0.5 (Fri Mar 19 2021)
+
+#### 🐛 Bug Fix
+
+- docs: fix wrong import in instructions [#3](https://github.com/storybookjs/testing-react/pull/3) ([@yannbf](https://github.com/yannbf))
+
+#### Authors: 1
+
+- Yann Braga ([@yannbf](https://github.com/yannbf))
+
+---
+
+# v0.0.4 (Fri Mar 19 2021)
+
+#### 🐛 Bug Fix
+
+- chore: update package name [#2](https://github.com/storybookjs/react-testing/pull/2) ([@yannbf](https://github.com/yannbf))
+
+#### Authors: 1
+
+- Yann Braga ([@yannbf](https://github.com/yannbf))
+
+---
+
+# v0.0.3 (Wed Mar 17 2021)
+
+#### ⚠️ Pushed to `main`
+
+- chore: add .env to gitignore (yannbf@gmail.com)
+- fix(docs): use correct lib name in tsdoc (yannbf@gmail.com)
+- chore(package): update project meta (yannbf@gmail.com)
+- docs(README): add project description (yannbf@gmail.com)
+
+#### Authors: 1
+
+- Yann Braga ([@yannbf](https://github.com/yannbf))
+
+---
+
+# v0.0.2 (Tue Mar 16 2021)
+
+#### ⚠️ Pushed to `main`
+
+- Rename to react-testing ([@shilman](https://github.com/shilman))
+- Initial version ([@yannbf](https://github.com/yannbf))
+
+#### Authors: 2
+
+- Michael Shilman ([@shilman](https://github.com/shilman))
+- Yann Braga ([@yannbf](https://github.com/yannbf))
+
+---
+
+# 0.0.1
+
+Initial version
---
+++ storybook-js/node_modules/@storybook/testing-react/dist/testing-react.cjs.development.js
@@ -0,0 +1,195 @@
+'use strict';
+
+Object.defineProperty(exports, '__esModule', { value: true });
+
+function _interopDefault (ex) { return (ex && (typeof ex === 'object') && 'default' in ex) ? ex['default'] : ex; }
+
+var clientApi = require('@storybook/client-api');
+var addons = require('@storybook/addons');
+var addons__default = _interopDefault(addons);
+
+function _extends() {
+  _extends = Object.assign || function (target) {
+    for (var i = 1; i < arguments.length; i++) {
+      var source = arguments[i];
+
+      for (var key in source) {
+        if (Object.prototype.hasOwnProperty.call(source, key)) {
+          target[key] = source[key];
+        }
+      }
+    }
+
+    return target;
+  };
+
+  return _extends.apply(this, arguments);
+}
+
+function _objectWithoutPropertiesLoose(source, excluded) {
+  if (source == null) return {};
+  var target = {};
+  var sourceKeys = Object.keys(source);
+  var key, i;
+
+  for (i = 0; i < sourceKeys.length; i++) {
+    key = sourceKeys[i];
+    if (excluded.indexOf(key) >= 0) continue;
+    target[key] = source[key];
+  }
+
+  return target;
+}
+
+addons__default.setChannel(addons.mockChannel());
+var globalStorybookConfig = {};
+/** Function that sets the globalConfig of your storybook. The global config is the preview module of your .storybook folder.
+ *
+ * It should be run a single time, so that your global config (e.g. decorators) is applied to your stories when using `composeStories` or `composeStory`.
+ *
+ * Example:
+ *```jsx
+ * // setup.js (for jest)
+ * import { setGlobalConfig } from '@storybook/testing-react';
+ * import * as globalStorybookConfig from './.storybook/preview';
+ *
+ * setGlobalConfig(globalStorybookConfig);
+ *```
+ *
+ * @param config - e.g. (import * as globalConfig from '../.storybook/preview')
+ */
+
+function setGlobalConfig(config) {
+  globalStorybookConfig = config;
+}
+/**
+ * Function that will receive a story along with meta (e.g. a default export from a .stories file)
+ * and optionally a globalConfig e.g. (import * from '../.storybook/preview)
+ * and will return a composed component that has all args/parameters/decorators/etc combined and applied to it.
+ *
+ *
+ * It's very useful for reusing a story in scenarios outside of Storybook like unit testing.
+ *
+ * Example:
+ *```jsx
+ * import { render } from '@testing-library/react';
+ * import { composeStory } from '@storybook/testing-react';
+ * import Meta, { Primary as PrimaryStory } from './Button.stories';
+ *
+ * const Primary = composeStory(PrimaryStory, Meta);
+ *
+ * test('renders primary button with Hello World', () => {
+ *   const { getByText } = render(<Primary>Hello world</Primary>);
+ *   expect(getByText(/Hello world/i)).not.toBeNull();
+ * });
+ *```
+ *
+ * @param story
+ * @param meta - e.g. (import Meta from './Button.stories')
+ * @param [globalConfig] - e.g. (import * as globalConfig from '../.storybook/preview') this can be applied automatically if you use `setGlobalConfig` in your setup files.
+ */
+
+function composeStory(story, meta, globalConfig) {
+  var _globalConfig;
+
+  if (globalConfig === void 0) {
+    globalConfig = globalStorybookConfig;
+  }
+
+  if (typeof story !== 'function') {
+    throw new Error("Cannot compose story due to invalid format. @storybook/testing-react expected a function but received " + typeof story + " instead.");
+  }
+
+  if (story.story !== undefined) {
+    throw new Error("StoryFn.story object-style annotation is not supported. @storybook/testing-react expects hoisted CSF stories.\n       https://github.com/storybookjs/storybook/blob/next/MIGRATION.md#hoisted-csf-annotations");
+  }
+
+  var finalStoryFn = function finalStoryFn(context) {
+    var _context$parameters$p = context.parameters.passArgsFirst,
+        passArgsFirst = _context$parameters$p === void 0 ? true : _context$parameters$p;
+
+    if (!passArgsFirst) {
+      throw new Error('composeStory does not support legacy style stories (with passArgsFirst = false).');
+    }
+
+    return story(context.args, context);
+  };
+
+  var combinedDecorators = [].concat(story.decorators || [], (meta == null ? void 0 : meta.decorators) || [], ((_globalConfig = globalConfig) == null ? void 0 : _globalConfig.decorators) || []);
+  var decorated = clientApi.defaultDecorateStory(finalStoryFn, combinedDecorators);
+  var defaultGlobals = Object.entries(globalConfig.globalTypes || {}).reduce(function (acc, _ref) {
+    var arg = _ref[0],
+        defaultValue = _ref[1].defaultValue;
+
+    if (defaultValue) {
+      acc[arg] = defaultValue;
+    }
+
+    return acc;
+  }, {});
+  var combinedParameters = clientApi.combineParameters(globalConfig.parameters || {}, meta.parameters || {}, story.parameters || {});
+
+  var combinedArgs = _extends({}, meta.args, story.args);
+
+  var composedStory = function composedStory(extraArgs) {
+    var config = {
+      id: '',
+      kind: '',
+      name: '',
+      argTypes: globalConfig.argTypes || {},
+      globals: defaultGlobals,
+      parameters: combinedParameters,
+      args: _extends({}, combinedArgs, extraArgs)
+    };
+    return decorated(config);
+  };
+
+  composedStory.args = combinedArgs;
+  composedStory.decorators = combinedDecorators;
+  composedStory.parameters = combinedParameters;
+  return composedStory;
+}
+/**
+ * Function that will receive a stories import (e.g. `import * as stories from './Button.stories'`)
+ * and optionally a globalConfig (e.g. `import * from '../.storybook/preview`)
+ * and will return an object containing all the stories passed, but now as a composed component that has all args/parameters/decorators/etc combined and applied to it.
+ *
+ *
+ * It's very useful for reusing stories in scenarios outside of Storybook like unit testing.
+ *
+ * Example:
+ *```jsx
+ * import { render } from '@testing-library/react';
+ * import { composeStories } from '@storybook/testing-react';
+ * import * as stories from './Button.stories';
+ *
+ * const { Primary, Secondary } = composeStories(stories);
+ *
+ * test('renders primary button with Hello World', () => {
+ *   const { getByText } = render(<Primary>Hello world</Primary>);
+ *   expect(getByText(/Hello world/i)).not.toBeNull();
+ * });
+ *```
+ *
+ * @param storiesImport - e.g. (import * as stories from './Button.stories')
+ * @param [globalConfig] - e.g. (import * as globalConfig from '../.storybook/preview') this can be applied automatically if you use `setGlobalConfig` in your setup files.
+ */
+
+function composeStories(storiesImport, globalConfig) {
+  var meta = storiesImport["default"],
+      stories = _objectWithoutPropertiesLoose(storiesImport, ["default", "__esModule"]); // Compose an object containing all processed stories passed as parameters
+
+
+  var composedStories = Object.entries(stories).reduce(function (storiesMap, _ref2) {
+    var key = _ref2[0],
+        story = _ref2[1];
+    storiesMap[key] = composeStory(story, meta, globalConfig);
+    return storiesMap;
+  }, {});
+  return composedStories;
+}
+
+exports.composeStories = composeStories;
+exports.composeStory = composeStory;
+exports.setGlobalConfig = setGlobalConfig;
+//# sourceMappingURL=testing-react.cjs.development.js.map
